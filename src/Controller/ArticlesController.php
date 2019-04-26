34fc1747841741f5c263513a234f239e2524cfa1
tisuchi <tisuchi@gmail.com>
2019-04-27 01:27:35 +0600

    Add twig

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -4,14 +4,16 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkExtraBundle\Controller\Controller;
 
-class ArticlesController {
+class ArticlesController extends Controller{
     /**
      * @Route("/")
      * @Method({"GET"})
      */
     public function index()
     {
-        return new Response('Hello world');
+        // return new Response('Hello world');
+        return $this->render('articles/index.html.twig');
     }
 }
