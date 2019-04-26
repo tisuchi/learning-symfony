824dbed8b7272df64df71839613010d7cceba0d0
tisuchi <tisuchi@gmail.com>
2019-04-27 01:23:30 +0600

    Add route bundle and use

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -6,6 +6,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ArticlesController {
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
     public function index()
     {
         return new Response('Hello world');
