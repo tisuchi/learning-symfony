a1d0e5b209c7db408fff16ea87e659d6bdaf15cf
tisuchi <tisuchi@gmail.com>
2019-04-27 01:21:05 +0600

    Add annotation package

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -2,6 +2,8 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class ArticlesController {
     public function index()
